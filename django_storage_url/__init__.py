5f42301bba1062d69c36a0210935d477b550573e
Angelo Dini <angelo.dini@divio.ch>
2020-09-01 11:26:43 +0200

    add configuration files from other addons

--- django_storage_url/__init__.py
+++ django_storage_url/__init__.py
@@ -5,6 +5,8 @@ from .base import (
 )
 from .backends import register_storage_class, get_storage_class
 
+__version__ = "0.4.0"
+
 __all__ = [
     "dsn_configured_storage",
     "dsn_configured_storage_class",
--- setup.py
+++ setup.py
@@ -1,15 +1,51 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+from setuptools import find_packages, setup
+
+from django_storage_url import __version__
+
+
+REQUIREMENTS = [
+    'django-storages',
+    'furl',
+]
+
+
+CLASSIFIERS = [
+    'Development Status :: 5 - Production/Stable',
+    'Environment :: Web Environment',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: BSD License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Framework :: Django',
+    'Framework :: Django :: 2.2',
+    'Framework :: Django :: 3.0',
+    'Framework :: Django :: 3.1',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development',
+    'Topic :: Software Development :: Libraries',
+]
+
 
 setup(
-    name="django-storage-url",
-    version="0.4.0",
+    name='django-storage-url',
+    version=__version__,
+    author='Jonathan Stoppani',
+    author_email='jonathan.stoppani@divio.com',
+    url='https://github.com/divio/django-storage-url',
+    license='MIT',
+    description='URL-based Django storage configuration',
+    long_description=open('README.rst').read(),
     packages=find_packages(),
-    url="https://github.com/divio/django-storage-url",
-    license="MIT",
-    author="Jonathan Stoppani",
-    author_email="jonathan.stoppani@divio.com",
-    description="URL-based Django storage configuration",
-    install_requires=["django-storages", "furl"],
-    extras_require={},
     include_package_data=True,
+    zip_safe=False,
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+    test_suite='tests.settings.run',
 )
